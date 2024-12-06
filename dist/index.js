88e21797bf9355374c65af828ca0b1980215774d
olvalaskevich <olvol269@gmail.com>
2024-12-06 12:57:27 +0300

    un MongoDb

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,13 +15,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const products_route_1 = require("./routes/products-route");
-const adresses_route_1 = require("./routes/adresses-route");
+const db_1 = require("./repositiries/db");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 const parserMiddleware = (0, body_parser_1.default)({});
 app.use(parserMiddleware);
 app.use('/products', products_route_1.productsRouter);
-app.use('/adresses', adresses_route_1.adressesRouter);
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+// app.use('/adresses', adressesRouter)
+const startApp = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, db_1.runDb)();
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
 });
+startApp();
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import {productsRouter} from "./routes/products-route";
 import {adressesRouter} from "./routes/adresses-route";
+import {runDb} from "./repositiries/db";
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -10,9 +11,14 @@ const port = process.env.PORT || 5000
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
 app.use('/products', productsRouter)
-app.use('/adresses', adressesRouter)
+// app.use('/adresses', adressesRouter)
+
+const startApp=async ()=>{
+    await runDb()
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+startApp()
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
